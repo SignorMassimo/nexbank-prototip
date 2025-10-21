de61e806fc07b738445ee8a054d38339cc8d9186
Z3XS0N <pervanisgenderli4@gmail.com>
2025-10-21 09:35:21 +0400

    u
    u

--- readme.md
+++ readme.md
@@ -1,2 +1,2 @@
 ## Web site link
-https://5f4bb5641b9a.ngrok-free.app
+https://d986dd8ffbaa.ngrok-free.app
--- server/src/main/java/com/zexson/signor_p/Security/CorsConfigurer.java
+++ server/src/main/java/com/zexson/signor_p/Security/CorsConfigurer.java
@@ -14,7 +14,7 @@ public class CorsConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4321", "http://192.168.100.157:4321", "https://unbelieving-uncalamitously-jackson.ngrok-free.dev")
+                        .allowedOrigins("http://localhost:4321", "http://192.168.100.157:4321", "https://d986dd8ffbaa.ngrok-free.app")
                         .allowedMethods("POST", "GET", "PUT")
                         .allowCredentials(true);
             }
